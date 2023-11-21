0a0db8e915430757e7ee2bd5446777e266e7dd8a
ketherball <121590283+ketherball@users.noreply.github.com>
2023-11-21 15:43:11 -0500

    add semi-colons

--- src/header/header.js
+++ src/header/header.js
@@ -1,12 +1,12 @@
-import { createHTML } from '../global-functions'
-import { events } from '../pub-sub'
-import './header.css'
+import { createHTML } from '../global-functions';
+import { events } from '../pub-sub';
+import './header.css';
 
 export const header = createHTML(`
     <div>
         <div id="header">TodoList</div>
     </div>
-`)
+`);
 
 const subHeader = createHTML(`
     <div id="sub-header">
@@ -16,56 +16,56 @@ const subHeader = createHTML(`
         <div class="slide" data-project data-sort="Important">Important</div>
         <div class="slide" data-project data-sort="Favorites">Favorites</div>
     </div>
-`)
+`);
 
-header.appendChild(subHeader)
+header.appendChild(subHeader);
 
 //Cache HTML
-const sortLinks = subHeader.children
+const sortLinks = subHeader.children;
 const getActiveSlide = function() {
-    const activeSlide = subHeader.querySelector('[data-active]')
-    return activeSlide
+    const activeSlide = subHeader.querySelector('[data-active]');
+    return activeSlide;
 }
 
 //Bind Events
 events.on('todolistSelected', function setHeader(selectedTodolist) {
     if (selectedTodolist.selectedProject === '') {
-        header.children[0].textContent = 'TodoList'
+        header.children[0].textContent = 'TodoList';
     } else {
-        header.children[0].textContent = selectedTodolist.selectedProject
+        header.children[0].textContent = selectedTodolist.selectedProject;
     }
-})
+});
 
 for (let link of sortLinks) {
     link.addEventListener('click', (e) => {
-        let activeSlide = getActiveSlide()
-        delete activeSlide.dataset.active
-        e.target.dataset.active = true//new active slide
+        let activeSlide = getActiveSlide();
+        delete activeSlide.dataset.active;
+        e.target.dataset.active = true; //new active slide
         let selectedTodolist = {
             selectedSort: link.dataset.sort,
             selectedProject: link.dataset.project
         }
-        events.emit('todolistSelected', selectedTodolist)
-    })
+        events.emit('todolistSelected', selectedTodolist);
+    });
 }
 
 events.on('sortSelected', function(selectedSort) {
-    let activeSlide = getActiveSlide()
-    delete activeSlide.dataset.active
+    let activeSlide = getActiveSlide();
+    delete activeSlide.dataset.active;
     for (let link of sortLinks) {
         if (link.dataset.sort === selectedSort) {
-            link.dataset.active = true
+            link.dataset.active = true;
             let selectedTodolist = {
                 selectedSort: link.dataset.sort,
                 selectedProject: link.dataset.project
             }
-            events.emit('todolistSelected', selectedTodolist)
+            events.emit('todolistSelected', selectedTodolist);
         }
     }
-})
+});
 
 events.on('projectSelected', function(selectedProject) {
     for (let link of sortLinks) {
-        link.dataset.project = selectedProject
+        link.dataset.project = selectedProject;
     }
-})
+});
